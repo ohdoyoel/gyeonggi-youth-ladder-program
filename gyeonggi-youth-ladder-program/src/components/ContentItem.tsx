64df40ec54941298dd3be123eae28b4ba02669a1
ohdoyoel <ohdoyoel@naver.com>
2025-01-17 15:13:54 +1300

    post page responsive design

--- gyeonggi-youth-ladder-program/src/components/ContentItem.tsx
+++ gyeonggi-youth-ladder-program/src/components/ContentItem.tsx
@@ -14,13 +14,13 @@ export default function ContentItem({
   return (
     <a
       href={link}
-      className="relative h-72 bg-cover bg-center transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-105 "
+      className="relative h-36 sm:h-72 w-[104px] sm:w-52 bg-cover bg-center transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-105 "
       style={{
         backgroundImage: `url('/programs/contents/${id}_${_id}.jpg')`,
       }}
     >
       <div className="absolute inset-0 bg-black opacity-60 backdrop-blur-md"></div>
-      <p className="absolute bottom-10 text-lg mx-4 font-bold text-white">
+      <p className="absolute bottom-2 sm:bottom-10 text-xs sm:text-lg mx-4 font-bold text-white">
         {title}
       </p>
     </a>
